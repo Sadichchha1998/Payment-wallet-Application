424d1f2cc55d3d216d586d55cbd56f778032c753
Rishav Choudhary <78720189+rishav015@users.noreply.github.com>
2023-02-22 20:53:47 +0530

    Add files via upload

---
+++ faithful-lock-5927/DigiWallet/src/main/java/com/masai/exception/BeneficiaryException.java
@@ -0,0 +1,15 @@
+package com.masai.exception;
+
+public class BeneficiaryException extends RuntimeException {
+
+	public BeneficiaryException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BeneficiaryException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
