424b0b160b34d9efc50b290793250ceee159d7de
NagArindam <inspkolkata12@gmail.com>
2023-02-24 23:24:29 +0530

    fp03_249_day4

--- DigiWallet/src/main/java/com/masai/controller/BeneficiaryController.java
+++ DigiWallet/src/main/java/com/masai/controller/BeneficiaryController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,31 +28,31 @@ public class BeneficiaryController {
 	@Autowired
 	private BeneficiaryService beneficiaryService;
 	
-	@PostMapping("/addBeneficiary")
-	public ResponseEntity<Beneficiary> addBeneficiaryMapping(Beneficiary beneficiary, String key)throws BeneficiaryException, CustomerException, WalletException{
+	@PostMapping("/addBeneficiary/{key}")
+	public ResponseEntity<Beneficiary> addBeneficiaryMapping(@RequestBody Beneficiary beneficiary,@PathVariable String key)throws BeneficiaryException, CustomerException, WalletException{
 		
 		return new ResponseEntity<Beneficiary>(beneficiaryService.addBeneficiary(beneficiary, key), HttpStatus.CREATED);
 		
 	}
 	
 	
-	@DeleteMapping("/deleteBeneficiary")
-	public ResponseEntity<Beneficiary> deleteBeneficiaryMapping(@Valid @RequestBody BeneficiaryDTO beneficiaryDTO,@RequestParam String key)throws BeneficiaryException, CustomerException, WalletException{
+	@DeleteMapping("/deleteBeneficiary/{key}")
+	public ResponseEntity<Beneficiary> deleteBeneficiaryMapping(@Valid @RequestBody BeneficiaryDTO beneficiaryDTO,@PathVariable String key)throws BeneficiaryException, CustomerException, WalletException{
 		
 		return new ResponseEntity<Beneficiary>(beneficiaryService.deleteBeneficiary(key, beneficiaryDTO), HttpStatus.OK);
 		
 	}
 	
 	
-	@GetMapping("/viewBeneficiary/name")
-	public ResponseEntity<Beneficiary> viewBeneficiaryMapping(@RequestParam String beneficiaryName,@RequestParam String key)throws BeneficiaryException, CustomerException{
+	@GetMapping("/viewBeneficiary/{beneficiaryName}/{key}")
+	public ResponseEntity<Beneficiary> viewBeneficiaryMapping(@PathVariable String beneficiaryName,@PathVariable String key)throws BeneficiaryException, CustomerException{
 		
 		return new ResponseEntity<Beneficiary>(beneficiaryService.viewBeneficiary(beneficiaryName, key), HttpStatus.OK);
 	}
 	
 	
-	@GetMapping("/viewAllBeneficiary")
-	public ResponseEntity<List<Beneficiary>> viewAllBeneficiaryMapping(@RequestParam String key)throws BeneficiaryException, CustomerException{
+	@GetMapping("/viewAllBeneficiary/{key}")
+	public ResponseEntity<List<Beneficiary>> viewAllBeneficiaryMapping(@PathVariable String key)throws BeneficiaryException, CustomerException{
 		
 		return new ResponseEntity<List<Beneficiary>>(beneficiaryService.viewAllBeneficiary(key), HttpStatus.OK);
 	}
